a31c9095a69b907b38db151755f37dc6d7ed6705
estillicon <paugracialopez86@yahoo.com>
2025-05-09 10:30:10 +0200

    cambios de enlaces

--- biblioteca-maricarmen/urls.py
+++ biblioteca-maricarmen/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", api.urls),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
